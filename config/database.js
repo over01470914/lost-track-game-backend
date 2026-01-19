5d58ceae4f16f3a831fbbfab7bce612c6a76ae00
GarbaGod <mingxun.xie@junkyardgames.com.cn>
2026-01-19 17:18:55 +0800

    修改数据库地址

--- config/database.js
+++ config/database.js
@@ -3,6 +3,6 @@ module.exports = {
     mongodb: {
         // 腾讯云ECS服务器上的MongoDB连接地址
         // 注意：实际部署时需要替换为真实的MongoDB连接地址
-        uri: 'mongodb://localhost:27017/trackingDB'
+        uri: 'mongodb://127.0.0.1:27017/trackingDB'
     }
 };
